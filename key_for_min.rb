4f812a37f4fd7e6ca5ae029a69781e5e1e2a6f42
Thomas Zobrosky <thomaszobrosky@gmail.com>
2019-11-18 02:59:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,11 +7,11 @@ def key_for_min_value(name_hash)
   #end 
   min_value = 0 
   min_key = nil 
-  name_hash.each{|key,value|
+  name_hash.each do |key,value|
     if value < min_value || value == 0
       min_value = value
       min_key = key
     end 
-  }
+  end 
   min_key
 end
